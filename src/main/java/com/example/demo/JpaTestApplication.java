d2a8f852ed75129f584ae6957f3ece2cd6514ec6
dlwnstn94 <dlwnstn9412@gmail.com>
2019-11-04 17:18:14 +0900

    spring boot 시작

---
+++ src/main/java/com/example/demo/JpaTestApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaTestApplication.class, args);
+		
+		System.out.println("jpa start");
+	}
+
+}
