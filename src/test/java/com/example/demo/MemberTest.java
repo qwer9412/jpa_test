f253038047e093dea23e5a6a87864b56a7fb1e1a
dlwnstn94 <dlwnstn9412@gmail.com>
2019-12-16 23:00:33 +0900

    paging, 전체 삭제, count test

--- src/test/java/com/example/demo/MemberTest.java
+++ src/test/java/com/example/demo/MemberTest.java
@@ -1,5 +1,8 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,71 +11,125 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
 public class MemberTest {
 	private static final Logger LOGGER = LoggerFactory.getLogger(MemberTest.class);
-	
+
 	@Autowired
 	private MemberRepository memberDao;
 
 	/*
-	 * Todo
-	 * db에 데이터가 실제로 있을 때 가정해서 수정하기
+	 * Todo db에 데이터가 실제로 있을 때 가정해서 수정하기
 	 */
-	
+
 	@Test
 	public void addTest() {
 		LOGGER.info("add test@@@@@@@@@@@@@@@@@@@");
 
-		Member member = new Member("leejunsu", "hello world");		
+		Member member = new Member("leejunsu", "hello world");
 		memberDao.save(member);
 
 		List<Member> memberList = memberDao.findAll();
 		System.out.println(memberList);
 	}
-	
+
 	@Test
 	public void getTest() {
 		LOGGER.info("get test@@@@@@@@@@@@@@@@@@@");
-		
+
 		Member newMember = new Member("leejunsu", "hello world");
 		memberDao.save(newMember);
-		
+
 		Member savedMember = memberDao.findById(1).get();
-		
+
 		System.out.println(savedMember);
 	}
-	
+
 	@Test
 	public void getAllTest() {
 		LOGGER.info("getAll test@@@@@@@@@@@@@@@@@@@");
-		
+
 		List<Member> newMemberList = new ArrayList<>();
 		newMemberList.add(new Member("leejunsu", "hello world"));
 		newMemberList.add(new Member("leejunsu2", "hello world2"));
 		memberDao.saveAll(newMemberList);
-		
+
 		List<Member> memberList = memberDao.findAll();
-		
+
 		System.out.println(memberList);
 	}
 
 	@Test
 	public void deleteTest() {
 		LOGGER.info("delete test@@@@@@@@@@@@@@@@@@@");
-		
-		Member newMember = new Member("a","b");
+
+		Member newMember = new Member("a", "b");
 		memberDao.save(newMember);
-		
+
 		List<Member> memberList = memberDao.findAll();
 		System.out.println(memberList);
-		
+
 		memberDao.deleteById(memberList.get(0).getId());
-		
+
 		memberList = memberDao.findAll();
 		System.out.println(memberList);
+	}
+
+	@Test
+	public void pagingTest() {
+		LOGGER.info("paging test@@@@@@@@@@@@@@@@@@@@@");
+
+		List<Member> memberList = new ArrayList<Member>();
+		for (int i = 1; i <= 20; i++) {
+			memberList.add(new Member("name" + i, "message" + i));
+		}
+
+		memberDao.saveAll(memberList);
+
+		for (int i = 0; i < 4; i++) {
+			PageRequest pageConfig = PageRequest.of(i, 5);
+			
+			List<Member> curPageMemberList = memberDao.findAll(pageConfig).getContent();
+			
+			LOGGER.debug("page" + i + ": ");
+			for(int idx = 0; idx < 5 ; idx++) {
+				assertEquals(memberList.get(i*5 + idx), curPageMemberList.get(idx));
+			}
+		}
+	}
+	
+	@Test
+	public void deleteAllTest() {
+		LOGGER.info("deleteAll test@@@@@@@@@@@@@@@");
+		
+		List<Member> memberList = new ArrayList<Member>();
+		for (int i = 1; i <= 20; i++) {
+			memberList.add(new Member("name" + i, "message" + i));
+		}
+		
+		memberDao.deleteAll();
+
+		List<Member> updateMemberList = memberDao.findAll();
+		
+		assertEquals(0, updateMemberList.size());
+	}
+	
+	@Test
+	public void countTest() {
+		LOGGER.info("count test@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+		
+		List<Member> memberList = new ArrayList<Member>();
+		for (int i = 1; i <= 20; i++) {
+			memberList.add(new Member("name" + i, "message" + i));
+		}
+		memberDao.saveAll(memberList);
+		
+		long memberDbCount = memberDao.count();
+		
+		assertEquals(memberList.size(), memberDbCount);
 	}
 }
